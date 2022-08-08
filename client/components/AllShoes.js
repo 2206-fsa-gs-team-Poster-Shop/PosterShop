72b707d904e7ceff2871109c35621e2e817c0b07
emrebsbydr <emrebasbaydar@gmail.com>
2022-08-08 17:33:24 -0400

    saved branches

--- client/components/AllShoes.js
+++ client/components/AllShoes.js
@@ -1,24 +1,112 @@
+// // import React, { Component } from 'react';
+// // import { Link } from 'react-router-dom';
+// // import { connect } from 'react-redux';
+// // import { fetchShoes } from '../store/redux/allShoes';
+// // import { getUserCart } from '../store/redux/cart';
+
+// // export class AllShoes extends Component {
+// //   componentDidMount() {
+// //     this.props.getShoes();
+// //     if(!!this.props.auth.id)
+// //     {
+// //       this.props.getUserCart(this.props.auth.id)
+// //     }
+// //   }
+
+// //   render() {
+// //     const allShoes = this.props.allShoes;
+// //     // console.log(this.props)
+// //     return (
+// //       <div id="all-shoes-view">
+// //         <h1>All Shoes</h1>
+// //         <div id="product-view">
+// //           {allShoes.map((shoe) => {
+// //             return (
+// //               <Link key={shoe.id} to={`/product/${shoe.id}`}>
+// //                 <div className="product">
+// //                   <img className="product-image" src={shoe.imageUrl} />
+// //                   <div>{shoe.name}</div>
+// //                 </div>
+// //               </Link>
+// //             );
+// //           })}
+// //         </div>
+// //       </div>
+// //     );
+// //   }
+// // }
+
+// // const mapStateToProps = (reduxState) => {
+// //   return {
+// //     allShoes: reduxState.allShoesReducer,
+// //     auth: reduxState.auth
+// //   };
+// // };
+
+// // const mapDispatchToProps = (dispatch) => ({
+// //   getShoes: () => dispatch(fetchShoes()),
+// //   getUserCart: (id) => dispatch(getUserCart(id))
+// // });
+
+// // export default connect(mapStateToProps, mapDispatchToProps)(AllShoes);
+
 // import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 // import { connect } from 'react-redux';
 // import { fetchShoes } from '../store/redux/allShoes';
-// import { getUserCart } from '../store/redux/cart';
 
 // export class AllShoes extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       brand: 'all',
+//     };
+//     this.clickHandler = this.clickHandler.bind(this);
+//   }
 //   componentDidMount() {
 //     this.props.getShoes();
-//     if(!!this.props.auth.id)
-//     {
-//       this.props.getUserCart(this.props.auth.id)
-//     }
+
+//   clickHandler(event) {
+//     const target = event.target.innerHTML;
+//     this.setState({
+//       brand: `${target}`,
+//     });
+//     console.log('state', this.state.brand);
+//     console.log('target value', target);
+
 //   }
 
 //   render() {
 //     const allShoes = this.props.allShoes;
-//     // console.log(this.props)
+//     console.log(allShoes);
+//     const {
+
+//     ler } = this;
+//     //  const filteredArr = allShoes.filter((shoe) => {
+//     //   if (this.state.brand === 'all') {
+//     //     return shoe
+//     //   } else if (shoe.brand === this.state.brand) {
+//     //     return shoe
+//     //   }
+//     // })
 //     return (
 //       <div id="all-shoes-view">
-//         <h1>All Shoes</h1>
+//         {/* FILTER BAR */}
+//         <div id="filter-bar">
+//           <div id="brand-filter">
+//             <a>Filter By</a>
+//           </div>
+
+//           <div id="brand-filter-content">
+//             <form>
+//               {allShoes.map((shoe) => {
+//                 return <a onClick={() => clickHandler(event)}>{shoe.name}</a>;
+//               })}
+//             </form>
+//           </div>
+//         </div>
+
+//         {/* FILTER BAR */}
 //         <div id="product-view">
 //           {allShoes.map((shoe) => {
 //             return (
@@ -39,16 +127,17 @@
 // const mapStateToProps = (reduxState) => {
 //   return {
 //     allShoes: reduxState.allShoesReducer,
-//     auth: reduxState.auth
 //   };
 // };
 
 // const mapDispatchToProps = (dispatch) => ({
 //   getShoes: () => dispatch(fetchShoes()),
-//   getUserCart: (id) => dispatch(getUserCart(id))
 // });
 
 // export default connect(mapStateToProps, mapDispatchToProps)(AllShoes);
+
+
+//================
 
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
@@ -65,23 +154,21 @@ export class AllShoes extends Component {
   }
   componentDidMount() {
     this.props.getShoes();
+  }
 
   clickHandler(event) {
     const target = event.target.innerHTML;
     this.setState({
-      brand: `${target}`,
+      brand:`${target}`,
     });
-    console.log('state', this.state.brand);
-    console.log('target value', target);
-
+    // console.log('state', this.state.brand);
+    // console.log('target value', target);
   }
 
   render() {
     const allShoes = this.props.allShoes;
     console.log(allShoes);
-    const { 
-    
-    ler } = this;
+    const { clickHandler } = this;
     //  const filteredArr = allShoes.filter((shoe) => {
     //   if (this.state.brand === 'all') {
     //     return shoe
@@ -91,7 +178,7 @@ export class AllShoes extends Component {
     // })
     return (
       <div id="all-shoes-view">
-        {/* FILTER BAR */}
+        {/* FILTER BAR /}
         <div id="filter-bar">
           <div id="brand-filter">
             <a>Filter By</a>
@@ -106,7 +193,7 @@ export class AllShoes extends Component {
           </div>
         </div>
 
-        {/* FILTER BAR */}
+        {/ FILTER BAR */}
         <div id="product-view">
           {allShoes.map((shoe) => {
             return (
@@ -123,7 +210,6 @@ export class AllShoes extends Component {
     );
   }
 }
-
 const mapStateToProps = (reduxState) => {
   return {
     allShoes: reduxState.allShoesReducer,
